5b2e5c59ee842ba99de8c6870ae61335a175375c
Biarslan <thebiarslan@gmail.com>
2024-02-12 21:57:25 +0300

    fix: prettier formatting

--- src/utils/validateUser.ts
+++ src/utils/validateUser.ts
@@ -6,29 +6,29 @@ export const validateUser = (user: unknown) => {
     errorArray.push('User should be a valid object');
   } else {
     if (!('username' in user && typeof user.username === 'string')) {
-        errorArray.push('Username should be type of string');
-      }
-      if (!('age' in user && typeof user.age === 'number')) {
-        errorArray.push("User's age should be type of number");
-      }
-      if (
-        !(
-          'hobbies' in user &&
-          Array.isArray(user.hobbies) &&
-          user.hobbies.every((hobby: unknown) => typeof hobby === 'string')
-        )
-      ) {
+      errorArray.push('Username should be type of string');
+    }
+    if (!('age' in user && typeof user.age === 'number')) {
+      errorArray.push("User's age should be type of number");
+    }
+    if (
+      !(
+        'hobbies' in user &&
+        Array.isArray(user.hobbies) &&
+        user.hobbies.every((hobby: unknown) => typeof hobby === 'string')
+      )
+    ) {
+      errorArray.push(
+        "User's hobbies should be and array of strings or empty array",
+      );
+    }
+    for (const key in user) {
+      if (!allowedKeys.includes(key)) {
         errorArray.push(
-          "User's hobbies should be and array of strings or empty array",
+          `User object should contain only this keys: ${allowedKeys.join(', ')}`,
         );
       }
-      for (const key in user) {
-        if (!allowedKeys.includes(key)) {
-          errorArray.push(
-            `User object should contain only this keys: ${allowedKeys.join(', ')}`,
-          );
-        }
-      }
+    }
   }
 
   return { isValidUser: errorArray.length > 0 ? false : true, errorArray };
